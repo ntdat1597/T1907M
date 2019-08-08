efa56e1327585b0760e92924e7c0f8c088799334
ntdat1597 <nguyentiendat19970501@gmail.com>
2019-08-08 21:11:21 +0700

    btvn

---
+++ somaxximumtrongmang.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+ 
+
+int max(int a, int b);
+int tim_so_lon_nhat;
+ 
+int main () 
+{
+ 
+   int a = 100;
+   int b = 200;
+   int max;
+    
+   
+ 
+   printf( "tim_so_lon_nhat %d : %d\n",tim_so_lon_nhat );
+ 
+   return 0;
+}
+int max(int a, int b) 
+{
+   int max; 
+   if (a < b)
+      max = a;
+      printf("tim_so_lon_nhat %d :%d\n",tim_so_lon_nhat);
+      return true;
+   
+      max = b;
+   return false; 
+}
