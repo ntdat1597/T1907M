4ff6f0a936f6951d5cdc0238c2a5bb819e64ec3e
ntdat1597 <53183723+ntdat1597@users.noreply.github.com>
2020-04-20 09:08:33 +0700

    lab1
    lab1

---
+++ program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Demo
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            string str;
+            int fb1, fb2, fb;
+            Console.Write("nhap so n: ");
+            //str = Console.ReadLine();
+            int n = Convert.ToInt32(Console.ReadLine());
+            fb = 0;
+            fb1 = 1;
+            fb2 = 1;
+            while(fb1 +  fb2 < n)
+            {
+                fb = fb1 + fb2;
+                fb2 = fb1;
+                fb1 = fb;
+            }
+            Console.Write("So fibonaci lon nhat nho hon n la: ");
+            Console.WriteLine(fb1);
+        }
+    }
+}
