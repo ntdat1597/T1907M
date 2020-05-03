39a6bd6de0e0ea073cfa51a282b865ded9f67e5f
ntdat1597 <53183723+ntdat1597@users.noreply.github.com>
2020-05-03 17:58:52 +0700

    Assignment 5
    Assignment 5

---
+++ Phone5.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApp.Assigment5
+{
+    public abstract class Phone
+    {
+        public abstract void insertPhone(string name,string phone);
+        public abstract void removePhone(string name);
+        public abstract void updatePhone(string name,string newphone);
+        public abstract void searchPhone(string name);
+    }
+}
---
+++ Program5.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using ConsoleApp.Lab2;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace ConsoleApp.Assigment5
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            PhoneBook phoneBook = new PhoneBook();
+            phoneBook.insertPhone("thang","0973006340");
+            phoneBook.insertPhone("hung","0984684184");
+            phoneBook.insertPhone("ngoc","0914646546");
+            phoneBook.removePhone("hung"); // hung delete
+            phoneBook.updatePhone("ngoc","091111111");
+            phoneBook.insertPhone("thang","0985851848");
+            phoneBook.show();
+            Console.WriteLine("Thong tin search phone");
+            phoneBook.searchPhone("thang");
+        }
+    }
+}
