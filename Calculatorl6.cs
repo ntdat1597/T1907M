6ff053846b4584aaa2248c31d8528808050f2628
ntdat1597 <53183723+ntdat1597@users.noreply.github.com>
2020-05-11 10:38:32 +0700

    lab6
    lab6

---
+++ Calculatorl6.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp.Session8
+{
+    public class Calculator
+    {
+        public static int Add(int a, int b)
+        {
+            Console.WriteLine(a+b);
+            return a + b;
+        }
+
+        public static int Subtract(int a, int b)
+        {
+            Console.WriteLine(a-b);
+            return a - b;
+        }
+        // hàm không có static
+    }
+}
---
+++ TaxCalcl6.cs
@@ -0,0 +1,37 @@
+﻿namespace ConsoleApp.Session8
+{
+    // tạo delegate mới trả về 1 hảm 
+    public delegate float TaxMoney(float salary);
+    public class TaxCalc
+    {
+        public static float VietNamTax(float salary)
+        {
+            return 10 * salary / 100;
+        }
+
+        public static float USATax(float salary)
+        {
+            return 15 * salary / 100;
+        }
+
+        public static float OtherTax(float salary)
+        {
+            return 5 * salary / 100;
+        }
+        // hàm trả về 1 hàm
+        public static TaxMoney GetTax(string country)
+        {
+            if (country == "VN")
+            {
+                return VietNamTax;
+            }else if (country == "USA")
+            {
+                return USATax;
+            }
+            else
+            {
+                return OtherTax;
+            }
+        }
+    }
+}
