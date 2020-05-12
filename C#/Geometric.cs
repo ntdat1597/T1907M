0e03a6cdaff1eae811022da63a86dffa3b231556
ntdat1597 <53183723+ntdat1597@users.noreply.github.com>
2020-05-12 10:27:55 +0700

    PracticalC#
    Practical C#

---
+++ C#/Geometric.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Demo.GeometryExample
+{
+    public class Geometric
+    {
+        public static void Main(string[] args)
+        {
+            Cylinder c1=new Cylinder();
+            Console.WriteLine("Enter the dimension of the cynlinder: ");
+            Console.WriteLine("Radius: ");
+            double radius = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Height: ");
+            double height = Convert.ToDouble(Console.ReadLine());
+            c1.Caculate(radius,height);
+            Console.WriteLine("Cylinder characteristics");
+            Console.WriteLine("Radius: "+radius);
+            Console.WriteLine("height: "+height);
+            c1.Result();
+        }
+    }
+}
